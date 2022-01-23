49c3ea125d7f4930c8009cd04bef142e69a60dfa
prembhaskal <pbhaskal@gmail.com>
2022-01-23 21:56:04 +0530

    some more leetcode problems

---
+++ ltc/linklist/revlinklist.go
@@ -0,0 +1,31 @@
+package revlinklist
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// prev -> curr -> next  ==> prev <- curr <- next
+// here head is the 1st element (with a value)
+func reverseList(head *ListNode) *ListNode {
+	var curr, prev, next *ListNode
+
+	prev = nil
+	curr = head
+	for curr != nil {
+		next = curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+
+	return prev
+}
---
+++ ltc/linklist/revlinklist_test.go
@@ -0,0 +1,48 @@
+package revlinklist
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReverseLinkedList1(t *testing.T) {
+	tcs := make([][]int, 0)
+	tcs = append(tcs, []int{1, 0, -1, 3, 4, 5, 9})
+	tcs = append(tcs, []int{})
+	tcs = append(tcs, []int{1})
+	tcs = append(tcs, []int{9, 5})
+	tcs = append(tcs, []int{9, 5, -1})
+
+	for _, ar := range tcs {
+		fmt.Println("new testcase")
+		lnklist := createLinkedList(ar)
+		printLinkedList(lnklist)
+
+		revlist := reverseList(lnklist)
+		printLinkedList(revlist)
+	}
+}
+
+func printLinkedList(head *ListNode) {
+	fmt.Println("********************************")
+	curr := head
+	for curr != nil {
+		fmt.Printf("Val: %d\n", curr.Val)
+		curr = curr.Next
+	}
+}
+
+// create linked list from array
+func createLinkedList(list []int) *ListNode {
+	var prev, curr *ListNode
+	head := &ListNode{}
+	prev = head
+	for _, v := range list {
+		curr = &ListNode{}
+		curr.Val = v
+		prev.Next = curr
+		prev = curr
+	}
+
+	return head.Next // return 1st element as head
+}
