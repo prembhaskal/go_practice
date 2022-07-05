d96e17713a03a20cbcc2e4dd6068d8b38e06e604
prembhaskal <pbhaskal@gmail.com>
2022-07-05 07:40:47 +0530

    lint errors

--- ltc/linklist/p138_copylist_random.go
+++ ltc/linklist/p138_copylist_random.go
@@ -9,42 +9,46 @@ package linklist
  * }
  */
 
-func copyRandomList(head *Node) *Node {
-    curr := head
-    
-    oldToNewMap := make(map[*Node]*Node)
-    
-    newPrev := &Node{} // dummy head
-    newHead := newPrev
-    
-    for curr != nil {
-        newCurr := &Node{
-            Val: curr.Val,
-            Next: curr.Next,
-        }
-        newPrev.Next = newCurr // point previous node to newly created node.
-        
-        oldToNewMap[curr] = newCurr
-        
-        // prepare for next round
-        curr = curr.Next
-        newPrev = newCurr
-    }
-    
-    
-    // update random address by navigating both list together
-    newCurr := newHead.Next
-    curr = head
-    for newCurr != nil {
-        oldRandom := curr.Random
-        newRandom := oldToNewMap[oldRandom]
-        newCurr.Random = newRandom
-        
-        // next round
-        curr = curr.Next
-        newCurr = newCurr.Next
-    }
-    
-    return newHead.Next
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
 }
 
+func copyRandomList(head *Node) *Node {
+	curr := head
+
+	oldToNewMap := make(map[*Node]*Node)
+
+	newPrev := &Node{} // dummy head
+	newHead := newPrev
+
+	for curr != nil {
+		newCurr := &Node{
+			Val:  curr.Val,
+			Next: curr.Next,
+		}
+		newPrev.Next = newCurr // point previous node to newly created node.
+
+		oldToNewMap[curr] = newCurr
+
+		// prepare for next round
+		curr = curr.Next
+		newPrev = newCurr
+	}
+
+	// update random address by navigating both list together
+	newCurr := newHead.Next
+	curr = head
+	for newCurr != nil {
+		oldRandom := curr.Random
+		newRandom := oldToNewMap[oldRandom]
+		newCurr.Random = newRandom
+
+		// next round
+		curr = curr.Next
+		newCurr = newCurr.Next
+	}
+
+	return newHead.Next
+}
