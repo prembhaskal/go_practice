5131c5ab6061dfc3f80a70b340430601e9abab6f
prembhaskal <pbhaskal@gmail.com>
2022-07-14 19:40:57 +0530

    simple stack implementation

---
+++ ltc/queue_stack/p_simple_stack.go
@@ -0,0 +1,49 @@
+package queue_stack
+
+import (
+	"fmt"
+)
+
+// https://go.dev/play/p/8ajtaWTZfPO
+func main() {
+	fmt.Println("Hello, 世界")
+
+	// stack as queue.
+	stck := newstk()
+	stck.push(1)
+	stck.push(2)
+	stck.push(3)
+	fmt.Printf("stck: %s\n", stck)
+	var val int
+	val = stck.pop()
+	fmt.Printf("pop: %d\n", val)
+	fmt.Printf("stck: %s\n", stck)
+}
+
+type stk struct {
+	arr []int
+}
+
+func newstk() *stk {
+	return &stk{
+		arr: make([]int, 0),
+	}
+}
+
+func (s *stk) push(val int) {
+	s.arr = append(s.arr, val)
+}
+
+func (s *stk) pop() int {
+	if len(s.arr) == 0 {
+		panic("empty stack")
+	}
+	n := len(s.arr)
+	val := s.arr[n-1]
+	s.arr = s.arr[:n-1]
+	return val
+}
+
+func (s *stk) String() string {
+	return fmt.Sprintf("%v", s.arr)
+}
