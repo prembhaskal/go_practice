471b57895c7ba91e8f76113f79acc64e25abbc15
prembhaskal <pbhaskal@gmail.com>
2022-07-23 08:21:20 +0530

    DFS with stack

--- ltc/queue_stack/p200_islands.go
+++ ltc/queue_stack/p200_islands.go
@@ -1,8 +1,96 @@
 package queue_stack
-
 func numIslands(grid [][]byte) int {
-	return doBFS(grid)
+	return doDFSWithStack(grid)
 }
+
+func doDFSWithStack(grid [][]byte) int {
+	m := len(grid)
+	n := len(grid[0])
+
+	visited := make([][]bool, len(grid))
+	for i := 0; i < m; i++ {
+		visited[i] = make([]bool, n)
+	}
+
+	total := 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			cell := islandcell{i, j}
+			if string(grid[i][j]) == "1" && !visited[i][j] {
+				stk := newislandstack()
+				stk.push(cell)
+				doDFSStack(visited, grid, stk)
+				total++
+			}
+		}
+	}
+
+	return total
+}
+
+func doDFSStack(visited [][]bool, grid [][]byte, stk *islandstack) {
+	for !stk.isempty() {
+		cell := stk.pop()
+		if visited[cell.i][cell.j] {
+			continue
+		}
+		visited[cell.i][cell.j] = true
+		addToStack(stk, grid, cell.i+1, cell.j)
+		addToStack(stk, grid, cell.i-1, cell.j)
+		addToStack(stk, grid, cell.i, cell.j+1)
+		addToStack(stk, grid, cell.i, cell.j-1)
+	}
+}
+
+func addToStack(stk *islandstack, grid [][]byte, i, j int) {
+	if i >= len(grid) || i < 0 {
+		return
+	}
+	if j >= len(grid[0]) || j < 0 {
+		return
+	}
+
+	if string(grid[i][j]) == "0" {
+		return
+	}
+
+	stk.push(islandcell{i, j})
+}
+
+type islandcell struct {
+	i int
+	j int
+}
+
+type islandstack struct {
+	arr []islandcell
+}
+
+func newislandstack() *islandstack {
+	return &islandstack{
+		arr: make([]islandcell, 0),
+	}
+}
+
+func (s *islandstack) push(cell islandcell) {
+	s.arr = append(s.arr, cell)
+}
+
+func (s *islandstack) pop() islandcell {
+	var icell islandcell
+	if s.isempty() {
+		return icell
+	}
+
+	icell = s.arr[len(s.arr)-1]
+	s.arr = s.arr[:len(s.arr)-1]
+	return icell
+}
+
+func (s *islandstack) isempty() bool {
+	return len(s.arr) == 0
+}
+
 
 func doBFS(grid [][]byte) int {
 	rows := len(grid)
@@ -23,8 +111,8 @@ func doBFS(grid [][]byte) int {
 			if string(grid[i][j]) == "0" {
 				continue
 			}
-			searchByBFS(grid, visited, i, j)
-			total++
+            searchByBFS(grid, visited, i, j)
+            total++
 		}
 	}
 
@@ -41,10 +129,10 @@ func searchByBFS(grid [][]byte, visited [][]bool, i, j int) {
 		if visited[nd.i][nd.j] {
 			continue
 		}
-		if string(grid[nd.i][nd.j]) == "0" {
-			continue
-		}
-		visited[nd.i][nd.j] = true
+        if string(grid[nd.i][nd.j]) == "0" {
+            continue
+        }
+        visited[nd.i][nd.j] = true
 
 		addToQueue(grid, nd.i+1, nd.j, nq)
 		addToQueue(grid, nd.i-1, nd.j, nq)
@@ -61,9 +149,9 @@ func addToQueue(grid [][]byte, i, j int, nq *nodequeue) {
 	if j < 0 || j >= len(grid[0]) {
 		return
 	}
-	if string(grid[i][j]) == "0" {
-		return
-	}
+    if string(grid[i][j]) == "0" {
+        return
+    }
 	nq.add(newnode(i, j))
 	return
 }
@@ -115,8 +203,8 @@ func doDFS(grid [][]byte) int {
 		for j := 0; j < len(grid[0]); j++ {
 			if string(grid[i][j]) == "1" && !seen[i][j] {
 				// fmt.Printf("checking node %d:%d\n", i , j)
-				visitConnected(i, j, grid, seen)
 				// seen[i][j] = true
+				visitConnected(i, j, grid, seen)
 				total++
 			}
 		}
