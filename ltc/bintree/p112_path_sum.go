ce3459aab9d9376c87cc6ecf210761bdaa22a6c5
prembhaskal <pbhaskal@gmail.com>
2022-08-07 09:47:00 +0530

    path sum.

---
+++ ltc/bintree/p112_path_sum.go
@@ -0,0 +1,22 @@
+package bintree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil { // if leaf, then compare sum.
+		return targetSum-root.Val == 0
+	}
+
+	// search in left node and right node
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
