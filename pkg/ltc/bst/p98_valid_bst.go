c5814181c76fa6ee0bb0a8d02fab0d03eb6f55be
prembhaskal <pbhaskal@gmail.com>
2022-10-09 20:38:29 +0530

    worked with in-order traverse

--- pkg/ltc/bst/p98_valid_bst.go
+++ pkg/ltc/bst/p98_valid_bst.go
@@ -9,8 +9,36 @@ package bst
  * }
  */
 func isValidBST(root *TreeNode) bool {
-	valid, _, _ := checkMinMax(root)
-	return valid
+	// valid, _, _ := checkMinMax(root)
+	// return valid
+	isvalid, _ := inorderBSTCheck(root, nil)
+	return isvalid
+}
+
+func inorderBSTCheck(root, prev *TreeNode) (bool, *TreeNode) {
+	if root == nil {
+		return true, prev
+	}
+	// if prev != nil {
+	//     fmt.Printf("root: %d, prev: %d\n", root.Val, prev.Val)
+	// } else {
+	//     fmt.Printf("root: %d, prev: nil\n", root.Val)
+	// }
+	var isvalid bool
+	// if root.Left != nil {
+	isvalid, prev = inorderBSTCheck(root.Left, prev)
+	if !isvalid {
+		return false, prev
+	}
+	// }
+	// if prev != nil {
+	//     fmt.Printf("curr: %d, prev: %d\n", root.Val, prev.Val)
+	// }
+	if prev != nil && prev.Val >= root.Val {
+		return false, prev
+	}
+
+	return inorderBSTCheck(root.Right, root)
 }
 
 func checkMinMax(root *TreeNode) (bool, int, int) {
