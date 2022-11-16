7b1e11ca9228d15f2da78f3d4d88bb9d80291ade
prembhaskal <pbhaskal@gmail.com>
2022-11-16 21:19:59 +0530

    same question with bfs

--- pkg/ltc/graph/p797_all_paths.go
+++ pkg/ltc/graph/p797_all_paths.go
@@ -1,6 +1,87 @@
 package graph
 
 func allPathsSourceTarget(graph [][]int) [][]int {
+	// graph is already adj. list
+
+	// source 0
+	// dest   n-1
+	// if we reach n-1, that means all previous levels are in path, we can maintain that in a stack or array.  that will path array.
+	//
+
+	dest := len(graph) - 1
+	// pseudo code
+
+	// get curr node from queue
+	//   add
+	// for curr node, list of neighbours
+	// for each neighbours,
+	// if neighbour == dest
+	//   add currpath to result
+
+	result := make([][]int, 0)
+
+	// return result.
+	queue := newp797q()
+	source := path797{node: 0, prevpath: []int{0}}
+	queue.add(source)
+
+	for queue.size() > 0 {
+		currpath := queue.poll()
+		currnode := currpath.node
+		prevpath := currpath.prevpath
+
+		if currnode == dest {
+			result = append(result, prevpath)
+		}
+
+		nextnodes := graph[currnode]
+		for _, next := range nextnodes {
+			// copy of currpath
+			copypath := make([]int, len(prevpath))
+			copy(copypath, prevpath)
+			copypath = append(copypath, next)
+			nextpath := path797{node: next, prevpath: copypath}
+
+			queue.add(nextpath)
+		}
+	}
+
+	return result
+}
+
+type path797 struct {
+	node     int
+	prevpath []int
+}
+
+type p797q struct {
+	ar []path797
+}
+
+func newp797q() *p797q {
+	return &p797q{
+		ar: make([]path797, 0),
+	}
+}
+
+func (q *p797q) add(item path797) {
+	q.ar = append(q.ar, item)
+}
+
+func (q *p797q) poll() path797 {
+	if q.size() == 0 {
+		panic("empty poll")
+	}
+	first := q.ar[0]
+	q.ar = q.ar[1:]
+	return first
+}
+
+func (q *p797q) size() int {
+	return len(q.ar)
+}
+
+func allPathsSourceTarget1(graph [][]int) [][]int {
 	n := len(graph)
 	allpaths := make([][]int, 0)
 
