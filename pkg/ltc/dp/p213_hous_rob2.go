e6a3aff4a65ffca7e2df9c43cd0cac436300b2f3
prembhaskal <pbhaskal@gmail.com>
2023-03-07 22:55:46 +0530

    new question

---
+++ pkg/ltc/dp/p213_hous_rob2.go
@@ -0,0 +1,34 @@
+package dp
+
+func rob2(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return nums[0]
+	}
+	return max213(rob1(nums[0:n-1]), rob1(nums[1:n]))
+}
+
+func rob1(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	mxrob := make([]int, n)
+	mxskip := make([]int, n)
+
+	mxrob[0] = nums[0]
+
+	for i := 1; i < n; i++ {
+		mxrob[i] = mxskip[i-1] + nums[i]
+		mxskip[i] = max213(mxskip[i-1], mxrob[i-1])
+	}
+
+	return max213(mxrob[n-1], mxskip[n-1])
+}
+
+func max213(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
