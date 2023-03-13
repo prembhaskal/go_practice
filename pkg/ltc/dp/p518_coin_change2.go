095c9506f7fbac2f3571e0a6cffaa057d5ac6173
prembhaskal <pbhaskal@gmail.com>
2023-03-13 11:15:30 +0530

    more optimization done here

--- pkg/ltc/dp/p518_coin_change2.go
+++ pkg/ltc/dp/p518_coin_change2.go
@@ -3,7 +3,79 @@ package dp
 func change(amount int, coins []int) int {
 	// fmt.Println("\n\n***************************\n\n")
 	// return f1mem(amount, coins)
-	return frec(amount, coins)
+	// return frec(amount, coins)
+	// return fiter(amount, coins)
+	// return fiterlessmem(amount, coins)
+	return fiteropt(amount, coins)
+}
+
+// optimized version.
+func fiteropt(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1 // base case
+
+	for i := 1; i < len(coins)+1; i++ {
+		for am := 1; am < amount+1; am++ {
+			// val := dp[am]
+			if am-coins[i-1] >= 0 {
+				dp[am] = dp[am] + dp[am-coins[i-1]]
+			}
+			// dp[am] = val
+		}
+	}
+	return dp[amount]
+}
+
+func fiterlessmem(amount int, coins []int) int {
+	// dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j]
+	// dp depends on self previous index or previous dp only.
+	prev := make([]int, amount+1)
+	prev[0] = 1
+
+	for i := 1; i < len(coins)+1; i++ {
+		curr := make([]int, amount+1)
+		curr[0] = 1
+		for am := 1; am < amount+1; am++ {
+			if am-coins[i-1] >= 0 {
+				curr[am] = curr[am-coins[i-1]] + prev[am]
+			} else {
+				curr[am] = prev[am]
+			}
+		}
+		prev = curr
+	}
+	return prev[amount]
+}
+
+func fiter(amount int, coins []int) int {
+	// dp[i][j] = count of combinations with 1st 'i' coins to make 'j' amount.
+	dp := make([][]int, 0)
+	for i := 0; i < len(coins)+1; i++ {
+		row := make([]int, amount+1)
+		dp = append(dp, row)
+	}
+
+	// base case
+	// with no coins, no combinations. dp[0][x] is set to ZERO automatically.
+	// with some coin > 0, if we reach amount = 0, then 1 combination is possible.
+
+	for i := 1; i < len(coins)+1; i++ {
+		dp[i][0] = 1
+	}
+
+	//   coins[i-1] because index is 0 based, the terminology is 1 based.
+	// dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j]
+	for i := 1; i < len(coins)+1; i++ {
+		for am := 1; am < amount+1; am++ {
+			if am-coins[i-1] >= 0 {
+				dp[i][am] = dp[i][am-coins[i-1]] + dp[i-1][am]
+			} else {
+				dp[i][am] = dp[i-1][am]
+			}
+		}
+	}
+
+	return dp[len(coins)][amount]
 }
 
 func frec(amount int, coins []int) int {
@@ -33,7 +105,10 @@ func f2mem(n, amount int, coins []int, mem [][]int) int {
 	if mem[n][amount] != -1 {
 		return mem[n][amount]
 	}
-
+	// classic choose/don't choose pattern in work here.
+	// choose (n-1)th coin, don't choose (n-1)th coin.
+	// f(n, amount - coins[n-1]...) means is , we use one coin with value 'coins[n-1]' and check combination with remaining amount
+	// f(n-1, amount, ...) means is , we don't use this coin and see if how many combinations we can make with this amount
 	mem[n][amount] = f2mem(n, amount-coins[n-1], coins, mem) +
 		f2mem(n-1, amount, coins, mem)
 	return mem[n][amount]
@@ -74,6 +149,7 @@ func f1mem(amount int, coins []int) int {
 	return f1(amount, coins, 0, mem)
 }
 
+// this works, but it is overly complicated.
 func f1(amount int, coins []int, coinidx int, mem [][]int) int {
 	// fmt.Printf("enter amount: %d, coinidx: %d\n", amount, coinidx)
 	if amount < 0 {
