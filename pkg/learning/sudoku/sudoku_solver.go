e3a22997b413226afeed37421bb2805a25d2af51
prembhaskal <pbhaskal@gmail.com>
2023-05-21 19:25:20 +0530

    finding lone cells working now

--- pkg/learning/sudoku/sudoku_solver.go
+++ pkg/learning/sudoku/sudoku_solver.go
@@ -2,6 +2,7 @@ package sudoku
 
 import (
 	"fmt"
+	"log"
 )
 
 // grid is divided into 9 parts, each part of 9 cells each
@@ -9,19 +10,106 @@ import (
 // [3, 4 , 5]
 // [6, 7 , 8]
 type Grid struct {
-	ar [][]int
+	Ar [][]int
 }
 
 type Set map[int]int
 
+var Debug = false
 
-// func SolveSingleCell(grid *Grid) {
+func SolveSingleCell(grid *Grid) {
+	for r := 0; r < 9; r++ {
+		for c := 0; c < 9; c++ {
+			val := grid.Ar[r][c]
 
-// }
+			// check only empty cells
+			if val != 0 {
+				continue 
+			}
+
+			relems := FindElementsInRow(grid, r)
+			celems := FindElementsInCol(grid, c)
+			partnum := FindGridPart(r, c)
+			pelems := FindElementsInPart(grid, partnum)
+
+			aelems := MergeSets1(relems, celems, pelems)
+
+			melems := FindMissingInSet(aelems)
+
+			if Debug {
+				log.Printf("row: %d, col: %d, miss: %v", r, c, melems)
+			}
+
+			if len(melems) == 1 {
+				missval := FindFirstKey(melems)
+				fmt.Printf("lone miss at part:%d row:%d, col:%d val: %d\n", partnum, r, c, missval)
+			}
+		}
+	}
+}
+
+func FindFirstKey(set Set) int {
+	for k, _ := range set {
+		return k
+	}
+	panic("empty set")
+}
+
+func FindMissingInSet(set Set) Set{
+	miss := make(map[int]int)
+	for n := 1; n <= 9; n++ {
+		if _, ok := set[n]; !ok {
+			miss[n] = 1
+		}
+	}
+
+	return miss
+}
+
+func MergeSets1(sets ...Set) Set {
+	elems := make(map[int]int)
+	for _, set := range sets {
+		for k, _ := range set {
+			elems[k] = 1
+		}
+	}
+	return elems
+}
+
+func FindElementsInRow(grid *Grid, rownum int) Set {
+	elems := make(map[int]int)
+	for c := 0; c < 9; c++ {
+		val := grid.Ar[rownum][c]
+		elems[val]++
+	}
+
+	return elems
+}
+
+func FindElementsInCol(grid *Grid, col int) Set {
+	elems := make(map[int]int)
+	for r := 0; r < 9; r++ {
+		val := grid.Ar[r][col]
+		elems[val]++
+	}
+
+	return elems
+}
 
 
-func FindElements(grid *Grid, partnum int) Set {
-	
+func FindElementsInPart(grid *Grid, partnum int) Set {
+	roff, coff := FindStartCell(partnum)
+	elems := make(map[int]int)
+	for r := 0; r < 3; r++ {
+		for c := 0; c < 3; c++ {
+			row := r + roff
+			col := c + coff
+			val := grid.Ar[row][col]
+			elems[val]++	
+		}
+	}
+
+	return elems
 }
 
 
@@ -31,7 +119,6 @@ func FindStartCell(partnum int) (int, int) {
 	if partnum < 0 || partnum > 8 {
 		panic(fmt.Sprintf("partnum out of bounds: %d", partnum))
 	}
-
 
 	row := partnum/3 // truncates towards 3
 	row = row * 3    // gets the boundary
--- pkg/learning/sudoku/sudoku_solver_test.go
+++ pkg/learning/sudoku/sudoku_solver_test.go
@@ -1,12 +1,27 @@
 package sudoku_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/prembhaskal/go_practice/pkg/learning/sudoku"
 )
+
+var cells1 = [][]int{
+	{6, 0, 0, 0, 4, 5, 9, 0, 8},
+	{0, 0, 0, 0, 7, 9, 0, 0, 0},
+	{5, 0, 0, 3, 0, 0, 0, 0, 0},
+
+	{1, 8, 0, 0, 0, 0, 3, 0, 0},
+	{3, 6, 0, 0, 0, 0, 0, 8, 4},
+	{0, 0, 4, 0, 0, 0, 0, 5, 2},
+
+	{0, 0, 0, 0, 0, 2, 0, 0, 3},
+	{0, 0, 0, 8, 3, 0, 0, 0, 0},
+	{7, 0, 8, 4, 5, 0, 0, 0, 6},
+}
 
 func TestFindGridParts(t *testing.T) {
 	// TODO - check the panic part
@@ -18,34 +33,75 @@ func TestFindGridParts(t *testing.T) {
 		c int
 		p int
 	}{
-		{0, 0, 0}, {0, 1, 0}, {0, 2, 0}, 
-		{1, 0, 0}, {1, 1, 0}, {1, 2, 0}, 
-		{2, 0, 0}, {2, 1, 0}, {2, 2, 0}, 
-		
-		{0, 3, 1}, {0, 4, 1}, {0, 5, 1}, 
-		{1, 3, 1}, {1, 4, 1}, {1, 5, 1}, 
-		{2, 3, 1}, {2, 4, 1}, {2, 5, 1}, 
-	
-		{0, 6, 2}, {0, 7, 2}, {0, 8, 2}, 
-		{1, 6, 2}, {1, 7, 2}, {1, 8, 2}, 
-		{2, 6, 2}, {2, 7, 2}, {2, 8, 2}, 
-		
+		{0, 0, 0},
+		{0, 1, 0},
+		{0, 2, 0},
+		{1, 0, 0},
+		{1, 1, 0},
+		{1, 2, 0},
+		{2, 0, 0},
+		{2, 1, 0},
+		{2, 2, 0},
 
-		{3, 0, 3}, {3, 1, 3}, {3, 2, 3}, 
-		{4, 0, 3}, {4, 1, 3}, {4, 2, 3}, 
-		{5, 0, 3}, {5, 1, 3}, {5, 2, 3}, 
-		
-		{3, 3, 4}, {3, 4, 4}, {3, 5, 4}, 
-		{4, 3, 4}, {4, 4, 4}, {4, 5, 4}, 
-		{5, 3, 4}, {5, 4, 4}, {5, 5, 4}, 
-		
-		{3, 6, 5}, {3, 7, 5}, {3, 8, 5}, 
-		{4, 6, 5}, {4, 7, 5}, {4, 8, 5}, 
-		{5, 6, 5}, {5, 7, 5}, {5, 8, 5},
+		{0, 3, 1},
+		{0, 4, 1},
+		{0, 5, 1},
+		{1, 3, 1},
+		{1, 4, 1},
+		{1, 5, 1},
+		{2, 3, 1},
+		{2, 4, 1},
+		{2, 5, 1},
 
-		{6, 6, 8}, {6, 7, 8}, {6, 8, 8}, 
-		{7, 6, 8}, {7, 7, 8}, {7, 8, 8}, 
-		{8, 6, 8}, {8, 7, 8}, {8, 8, 8}, 
+		{0, 6, 2},
+		{0, 7, 2},
+		{0, 8, 2},
+		{1, 6, 2},
+		{1, 7, 2},
+		{1, 8, 2},
+		{2, 6, 2},
+		{2, 7, 2},
+		{2, 8, 2},
+
+		{3, 0, 3},
+		{3, 1, 3},
+		{3, 2, 3},
+		{4, 0, 3},
+		{4, 1, 3},
+		{4, 2, 3},
+		{5, 0, 3},
+		{5, 1, 3},
+		{5, 2, 3},
+
+		{3, 3, 4},
+		{3, 4, 4},
+		{3, 5, 4},
+		{4, 3, 4},
+		{4, 4, 4},
+		{4, 5, 4},
+		{5, 3, 4},
+		{5, 4, 4},
+		{5, 5, 4},
+
+		{3, 6, 5},
+		{3, 7, 5},
+		{3, 8, 5},
+		{4, 6, 5},
+		{4, 7, 5},
+		{4, 8, 5},
+		{5, 6, 5},
+		{5, 7, 5},
+		{5, 8, 5},
+
+		{6, 6, 8},
+		{6, 7, 8},
+		{6, 8, 8},
+		{7, 6, 8},
+		{7, 7, 8},
+		{7, 8, 8},
+		{8, 6, 8},
+		{8, 7, 8},
+		{8, 8, 8},
 	}
 
 	for _, tt := range testdata {
@@ -55,14 +111,20 @@ func TestFindGridParts(t *testing.T) {
 }
 
 func TestFindStartCell(t *testing.T) {
-	testdata := []struct{
+	testdata := []struct {
 		part int
-		row int
-		col int
+		row  int
+		col  int
 	}{
-		{0, 0, 0}, {1, 0, 3}, {2, 0, 6},
-		{3, 3, 0}, {4, 3, 3}, {5, 3, 6},
-		{6, 6, 0}, {7, 6, 3}, {8, 6, 6},
+		{0, 0, 0},
+		{1, 0, 3},
+		{2, 0, 6},
+		{3, 3, 0},
+		{4, 3, 3},
+		{5, 3, 6},
+		{6, 6, 0},
+		{7, 6, 3},
+		{8, 6, 6},
 	}
 
 	for _, tt := range testdata {
@@ -70,4 +132,56 @@ func TestFindStartCell(t *testing.T) {
 		assert.Equal(t, ar, tt.row)
 		assert.Equal(t, ac, tt.col)
 	}
+}
+
+// TODO add proper asse
+func TestFindElementsInPart(t *testing.T) {
+	sampleGrid := &sudoku.Grid{
+		Ar: cells1,
+	}
+
+	for i := 0; i < 9; i++ {
+		fmt.Printf("part: %d\n", i)
+		elems := sudoku.FindElementsInPart(sampleGrid, i)
+		fmt.Printf("elems are %v\n", elems)
+	}
+
+	for r := 0; r < 9; r++ {
+		fmt.Printf("vals in row: %d\n", r)
+		elems := sudoku.FindElementsInRow(sampleGrid, r)
+		fmt.Printf("elems are %v\n", elems)
+	}
+
+	for c := 0; c < 9; c++ {
+		fmt.Printf("vals in column: %d\n", c)
+		elems := sudoku.FindElementsInCol(sampleGrid, c)
+		fmt.Printf("elems are %v\n", elems)
+	}
+}
+
+
+func TestMergeSets1(t *testing.T) {
+	sampleGrid := &sudoku.Grid{Ar:cells1}
+
+	relems := sudoku.FindElementsInRow(sampleGrid, 0)
+	celems := sudoku.FindElementsInCol(sampleGrid, 0)
+	aelems := sudoku.FindElementsInPart(sampleGrid, 0)
+
+	elems := sudoku.MergeSets1(relems, celems, aelems)
+
+	expelems := map[int]int{
+		0:1, 1:1, 3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1,
+	}
+
+	assert.Equal(t, elems, sudoku.Set(expelems))
+
+	expmiss := map[int]int{2:1}
+	miss := sudoku.FindMissingInSet(elems)
+	assert.Equal(t, sudoku.Set(expmiss), miss)
+}
+
+func TestSolveSingleCell(t *testing.T) {
+	sampleGrid := &sudoku.Grid{Ar: cells1}
+	// sudoku.Debug = true
+	sudoku.SolveSingleCell(sampleGrid)
 }
