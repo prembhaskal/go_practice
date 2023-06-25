a80e2b0f1fd5d52188a74c317912339537f2a6cc
prembhaskal <pbhaskal@gmail.com>
2023-06-25 19:20:23 +0530

    cleaner code for LIS with tail table intuition at the top.

--- pkg/ltc/array/p300_longincsubseq.go
+++ pkg/ltc/array/p300_longincsubseq.go
@@ -1,57 +1,52 @@
 package array
 
+
+// DP[i] = {max{DP[j]]} where j = i-1 to 0 and A[j] < A[i]}
+// we use tail table to search in the smallest element larger than current element.
+
+// tail table intuition
+// tt[i] = x, it means 'x' is the smallest element using which we can have LIS of length 'i'
+// whenever new number 'y' comes in, we check smallest number bigger than or equal to 'x', aka search for lower_bound
+
 func lengthOfLIS(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-
-	// init tail table, where index = len of subsequence.
-	tt := make([]int, 2)
-
-	// DP[i] = {max{DP[j]]} where j = i-1 to 0 and A[j] < A[i]}
-	// we use tail table to search in the smallest element larger than current element.
-
-	// tail table intuition
-	// tt[i] = x, it means 'x' is the smallest element using which we can have LIS of length 'i'
-	// whenever new number 'y' comes in, we check smallest number bigger than or equal to 'x', aka search for lower_bound
-
-
-	// initial condition
-	tt[1] = nums[0]
-
-	for i := 1; i < len(nums); i++ {
-		last_elem := tt[len(tt)-1]
-		if nums[i] > last_elem {
-			tt = append(tt, nums[i])
-			continue
-		}
-
-		// else search element in tail table
-		elem_idx := binSearchCeilIndex(tt, nums[i])
-		tt[elem_idx] = nums[i]
-
-		// fmt.Printf("nums[%d]=%d, elem_idx=%d\n", i, nums[i], elem_idx)
-	}
-
-	return len(tt) - 1
+    if len(nums) == 0 {
+        return 0
+    }
+    
+    tt := make([]int, 0)
+    tt = append(tt, nums[0])
+    ln := 1
+    
+    for i := 1; i < len(nums); i++ {
+        last_elem := tt[ln-1]
+        if nums[i] > last_elem {
+            tt = append(tt, nums[i])
+            ln++
+        } else { // search element in tail table
+            elem_idx := lower_bound(tt, nums[i])
+            tt[elem_idx] = nums[i]
+        }
+    }
+    
+    return ln
 }
 
-// if number == key, return its index.
-// else search smallest number > key and return its index.
-func binSearchCeilIndex(tt []int, key int) int {
-	low := 1
-	high := len(tt) - 1
-	for low < high {
-		mid := (low + high) / 2
-		// if tt[mid] == key {
-		//     return mid
-		// }
-		if tt[mid] < key {
-			low = mid + 1
-		} else {
-			high = mid
-		}
-	}
-
-	return high
+// search for smallest idx, such that ar[idx] >= key
+func lower_bound(ar []int, key int) int {
+    low := 0
+    high := len(ar) - 1
+    ans := -1
+    for low <= high {
+        mid := (low + high) / 2
+        if ar[mid] == key {
+            return mid
+        }
+        if ar[mid] >= key {
+            ans = mid
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return ans
 }
