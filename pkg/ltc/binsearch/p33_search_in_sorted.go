b1b73690eafd813881f6e71a82351b196c4775da
premkumar bhaskal <pbhaskal@gmail.com>
2024-05-15 12:37:21 +0530

    yet another approach

--- pkg/ltc/binsearch/p33_search_in_sorted.go
+++ pkg/ltc/binsearch/p33_search_in_sorted.go
@@ -130,3 +130,42 @@ func search3(nums []int, target int) int {
 	}
 	return -1
 }
+
+// one more approach
+func search4(nums []int, target int) int {
+    low := 0;
+    high := len(nums)-1;
+
+    for low < high {
+        mid := low + (high - low) / 2;
+
+        if nums[mid] == target {
+            return mid
+        }
+
+        // check if left is sorted
+        if nums[low] < nums[mid] {
+            // check if element lies in sorted part
+            if target >= nums[low] && target < nums[mid] {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        } else if nums[low] == nums[mid] { // boundary cases with 2 elements and rotated.
+            // we already matched mid, so now check in high
+            low = mid + 1
+        } else { // right is sorted
+            // elements lies in sorted right part
+            if target > nums[mid] && target <= nums[high] {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+    }
+    if nums[low] == target {
+        return low
+    }
+
+    return -1
+}
