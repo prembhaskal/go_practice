92207c253571e4c53d4168452b3ff27941fb2751
premkumar bhaskal <pbhaskal@gmail.com>
2024-05-25 23:44:27 +0530

    more optimal freq compare instead of maps

--- pkg/ltc/slidewin/p76_minwindow_substr.go
+++ pkg/ltc/slidewin/p76_minwindow_substr.go
@@ -2,7 +2,111 @@ package slidewin
 
 import "fmt"
 
+// sliding window with matching using comparing unique counts (instead of map hence faster)
+// TODO make it readable
 func minWindow(s string, t string) string {
+	// make map of t
+	freq := make(map[byte]int)
+	unique := 0
+	for i := 0; i < len(t); i++ {
+		freq[t[i]]++
+		if freq[t[i]] == 1 {
+			unique++
+		}
+	}
+
+	if len(t) > len(s) {
+		return ""
+	}
+
+	// init slide window
+	actual := 0
+	slide := make(map[byte]int)
+	for i := 0; i < len(t); i++ {
+		slide[s[i]]++
+	}
+
+	for k, v := range slide {
+		if freq[k] > 0 && v >= freq[k] {
+			actual++
+		}
+	}
+
+	minlen := 1000000
+
+	// track matching substring
+	ms := 0
+	me := -1
+
+	// iterate and check
+	cs := 0
+	ce := len(t) - 1
+	// fmt.Println("exp map")
+	// printmap(freq)
+	for ce < len(s) {
+		// check current state and record if smallest
+		// fmt.Println("actual map")
+		// printmap(slide)
+		// if isWindowSubstring(freq, slide) {
+		// fmt.Printf("matching start cs: %d, ce: %d, unique: %d, actual: %d\n", cs, ce, unique, actual)
+		if unique == actual {
+			currlen := ce - cs + 1
+			// fmt.Printf("matched map, cs: %d, ce: %d, currlen: %d, minlen: %d \n", cs, ce, currlen, minlen)
+			if currlen < minlen {
+				minlen = currlen
+				ms = cs
+				me = ce
+			}
+		}
+
+		// inc end
+		ce++
+		if ce == len(s) {
+			break
+		}
+		slide[s[ce]]++
+		if freq[s[ce]] > 0 && slide[s[ce]] == freq[s[ce]] {
+			actual++
+		}
+		// fmt.Printf("slide map ce: %d\n", ce)
+		// printmap(slide)
+
+		// remove from start if not matching or if already present
+		for cs < ce {
+			startch := s[cs]
+			if freq[startch] == 0 {
+				cs++
+				slide[startch]--
+			} else if slide[startch] > freq[startch] {
+				cs++
+				slide[startch]--
+			} else {
+				// fmt.Printf("breaking cs inc at cs: %d, s[cs]: %c, ce: %d, s[ce]: %c \n", cs, startch, ce, s[ce])
+				// update actual
+				break
+			}
+		}
+
+		// fmt.Printf("matching cs: %d, ce: %d, unique: %d, actual: %d\n", cs, ce, unique, actual)
+
+		if unique == actual {
+			currlen := ce - cs + 1
+			// fmt.Printf("matched map, cs: %d, ce: %d, currlen: %d, minlen: %d \n", cs, ce, currlen, minlen)
+			if currlen < minlen {
+				minlen = currlen
+				ms = cs
+				me = ce
+			}
+		}
+	}
+
+	// fmt.Printf("breaking at cs: %d, ce: %d\n", cs, ce)
+
+	return s[ms : me+1]
+}
+
+// sliding window with matching using comparing maps
+func minWindow1(s string, t string) string {
 	// make map of t
 	freq := make(map[byte]int)
 	for i := 0; i < len(t); i++ {
